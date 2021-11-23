a36a97a3e7fa514a785f22d58b60df1cf1884f54
Daniel <Da_919@hotmail.com>
2021-11-23 14:43:37 -0500

    fix: se arreglan errores en imports de avance

--- models/avance/avance.js
+++ models/avance/avance.js
@@ -1,40 +1,37 @@
-const { from } = require("core-js/core/array");
-const { Schema } = require("js-yaml");
+import mongoose from "mongoose";
+import { ProjectModel } from "../proyecto/proyecto.js";
+import { UserModel } from "../usuario/usuario.js";
 
-importar  mangosta  de  'mangosta' ;
-importar  {  ProjectModel  }  from  '../proyecto/proyecto.js' ;
-importar  {  UserModel  }  from  '../usuario/usuario.js' ;
+const { Schema, model } = mongoose;
 
-const  { Esquema , modelo }  =  mongoose ;
-
-//se crea el esquema de avance 
+//se crea el esquema de avance
 
 const avanceSchema = new Schema({
-    fecha: {
-      type: Date,
-      required: true,
-    },
-    descripcion: {
+  fecha: {
+    type: Date,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  observaciones: [
+    {
       type: String,
-      required: true,
     },
-    observaciones: [
-      {
-        type: String,
-      },
-    ],
-    proyecto: {
-      type: Schema.Types.ObjectId,
-      ref: ProjectModel, //referencia el modelo de proyectos
-      required: true,
-    },
-    creadoPor: {
-      type: Schema.Types.ObjectId,
-      ref: UserModel, // referencia el modelo de usuarios
-      required: true,
-    },
-  });
-  
-  const ModeloAvance = model('Avance', avanceSchema); //modelo de avances
-  
-  export { ModeloAvance };
+  ],
+  proyecto: {
+    type: Schema.Types.ObjectId,
+    ref: ProjectModel, //referencia el modelo de proyectos
+    required: true,
+  },
+  creadoPor: {
+    type: Schema.Types.ObjectId,
+    ref: UserModel, // referencia el modelo de usuarios
+    required: true,
+  },
+});
+
+const ModeloAvance = model("Avance", avanceSchema); //modelo de avances
+
+export { ModeloAvance };
