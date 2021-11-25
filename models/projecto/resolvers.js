cfcfb5c4445689ff6e1239ad4f3ee0f964fa58e7
Daniel <Da_919@hotmail.com>
2021-11-24 20:19:10 -0500

    se hace los query de proyectos

--- models/projecto/resolvers.js
+++ models/projecto/resolvers.js
@@ -1,9 +1,15 @@
+import { ProjectModel } from "../proyecto/project";
 const resolversProyecto = {
   Query: {
-    Proyectos:  {
-      const proyecto
+    Proyectos: async (parent, args) => {
+      const proyectosEncontrados = await ProjectModel.find();
+      return proyectosEncontrados;
     },
-    Proyecto: {},
+    Proyecto: async (parent, args) => {
+      const proyectoEncontrado = await ProjectModel.findOne({
+        _id: _id,
+      });
+    },
   },
   Mutation: {},
 };
