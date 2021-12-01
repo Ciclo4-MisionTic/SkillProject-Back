5a137445791923cc92b601a6c4557c2bac6fba15
Evelin Mendoza <90517661+EveMendo@users.noreply.github.com>
2021-12-01 14:54:18 -0500

    Update resolvers.js
    Agrego resolvers de avance

---
+++ models/avance/resolvers.js
@@ -0,0 +1,52 @@
+import { ModeloAvance } from './avance.js';
+
+const resolversAvance = {
+  Query: {
+    Avances: async (parent, args) => {
+      const avances = await ModeloAvance.find().populate('proyecto').populate('creadoPor');
+      return avances;
+    },
+    filtrarAvance: async (parents, args) => {
+      const avanceFiltrado = await ModeloAvance.find({ proyecto: args._id })
+        .populate('proyecto')
+        .populate('creadoPor');
+      return avanceFiltrado;
+    },
+  },
+  Mutation: {
+    crearAvance: async (parents, args) => {
+      const avanceCreado = ModeloAvance.create({
+        fecha: args.fecha,
+        descripcion: args.descripcion,
+        proyecto: args.proyecto,
+        creadoPor: args.creadoPor,
+      });
+      return avanceCreado;
+    },
+    editarObservaciones: async (parents, args) => {
+      const observacionEditado = await ModeloAvance.UserModel.findByIdAndUpdate(
+          args._id,
+          {
+            observaciones: args.observaciones,
+          },
+          { new: true }
+        );
+  
+        return observacionEditado;
+    },
+    editarDescripcion: async (parents, args) => {
+      const descripcionEditado = await ModeloAvance.UserModel.findByIdAndUpdate(
+          args._id,
+          {
+            descripcion: args.descripcion,
+          },
+          { new: true }
+        );
+  
+        return descripcionEditado;
+    },
+    
+  },
+};
+
+export { resolversAvance };
