df565980bd5324c71ace7bfad8f9490635bd272f
antonio <45393639+JoseA94@users.noreply.github.com>
2021-12-09 19:04:30 -0500

    actualizaciones en el back para el query inscripciones y crearInscripción

--- models/inscripcion/tipos.js
+++ models/inscripcion/tipos.js
@@ -29,9 +29,7 @@ const tiposInscripcion = gql`
 
   type Mutation {
     crearInscripcion(
-      estado: Enum_EstadoInscripcion!
       proyecto: String!
-      estudiante: String!
     ): Inscripcion
 
     aprobarInscripcion(id: String!): Inscripcion
